d25c21bc187d44cf44843a941e870b6aca01fa2d
Christian BOGRO <cb@link2b.fr>
2019-07-25 12:30:18 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='phone_email_verifier',
+    version='0.0.1',
+    description='Validation of the email or international or local telephone number',
+    long_description=open('README.md').read(),
+    author='Christian BOGRO',
+    author_email='bogrolcr@gmail.com',
+    license='MIT',
+    url='https://github.com/Bogro/phone_email_verifier',
+    download_url='https://github.com/Bogro/phone_email_verifier/tarball/master',
+    keywords = ['email', 'phone number', 'filter', 'blocking', 'text'],
+)
