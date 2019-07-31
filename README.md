69713a6e16ae167f097ff2b1ce3677c62df2a84d
Christian BOGRO <bogrolcr@gmail.com>
2019-07-31 22:14:35 +0000

    update treatment phone

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PHONE & EMAIL Verifier V0.0.3
+# PHONE & EMAIL Verifier
 
 ## Description
  Validation of the email or international or local telephone number.
@@ -24,50 +24,149 @@ For direct downloads on github you do not need to follow the process above
 
 ## Usage
 
-
-    >>> from phone_email_verifier import phone_email_verifier
-
-    >>> filter = phone_email_verifier()
-
-    
-
    ### E-mail
 
 E-mail list
 
-    email = ['test@mail.tx', 'demo@mail.test', 'e.mail@mailer.com']
+    from phone_email_verifier.email_verifier import email_verifier as email_v
 
-    >>> filter.set_email_list(email, )
 
-    >>> filter.exec()
+    email = email_v()
+
+    email_list = ['test@mail.tx', 'demo@mail.test', 'e.mail@mailer.com']
+
+    >>> email.set_email_list(email_list) 
+
+    or
+
+    >>> email.set_email_in_file(file_name)
+
+    >>> email.exec()
 
     {'OK': ['test@mail.tx', 'e.mail@mailer.com'], 'ERROR': ['demo@mail.test']}
+
+#### Remarque:
+Il est possible de faire une restricrion sur le pays en faisant:
+
+    >>> email.set_email_list(email, 'FR')
+
+    or
+
+    >>> email.set_email_in_file(file_name, 'FR')
+
+et célà ferra un filtre sur tous email se terminant pas 
+    
+    .fr
+
+si le fichier utiliser est un CSV indique la colonne ou trouver le email
+
+    >>> email.set_email_in_file(file_name, colum=0)
+
+    or
+
+    >>> email.set_email_in_file(file_name, 'FR', 0)
+
+##### Reamarque
+    Les types de fichier utilisable sont:
+    CSV et TXT
+    
 
    ### Phone
 
 Number phone list 
     
-    #simple use
 
-    phone = ['+22547000000', '+225-47-00-00-00', '+225 47 00 00 00 00', '+33 00 25 00 11 00', '47 02 00 00']
+    from phone_email_verifier.phone_verifier import phone_verifier as phone_v
 
-    >>> filter.set_phone_list(phone)
+    phone = phone_v()
 
-    {'OK': ['+22547000000', '+22547000000', '+2254700000000', '+330025001100'], 'ERROR': ['47020000']}
-
+    phone_list = ['+22547000000', '+225-47-00-00-00', '+225 47 00 00 00 00', '+33 00 25 00 11', '47 02 00 00']
 
 
-a file
+#### simple use
 
-    #simple use of a contact list
+    >>> filter.set_phone_list(phone_list)
 
-    name_list = list_phone.csv
+    or
 
-    >>> filter.set_phone_in_file(name_list, colum=0) # colum is column of numbers
+    >>> filter.set_phone_in_file(file_name)
 
     >>> filter.exec()
 
-    {'OK': ['+22547000000', '22547000010'], 'ERROR': ['030020585', '4578', '33024558452', '55552222', '658945898']}
+    {'OK': ['+22547000000', '+22547000000', '+2254700000000', '+3300250011'], 'ERROR': ['47020000']}
+
+
+#### Advance use
+
+Il est possible d'apporté une précision avec le nom du pays et sont code indentifiant
+
+    >>> filter.set_phone_list(phone_list, 'FR', '+33')
+
+    or
+
+    >>> filter.set_phone_in_file(name_list, 'FR', '+33', colum=0) # colum is column of numbers
+
+    >>> filter.exec()
+
+    {'OK': ['+3300250011'], 'ERROR': ['47020000', '+22547000000', '+22547000000', '+2254700000000']}
 
 
 It is important to specify the number column when the file is a csv file.
+
+
+# Pour le dictionnaire de code identifient des pays
+
+Vous pourvez ajouté ou reduire la liste en function de votre utilisation.
+Je vous conseille de reduire en fonction de votre utilisation, pour rendre rapide l'execution
+
+## Comment ajouter
+
+Pour ajouter, aller a la ligne et
+
+le nom du pays,abreviation du nom,code identifient (+225|225|0225), longuer du numéro sans le code identifient et le code local, le code local s'il existe,
+
+Exemple:
+Nom du pays
+--------------
+    France
+
+Abreviation du nom
+------------------
+    FR
+
+Code identifient
+----------------
+    +33|33|0033
+
+Longueur du numéro
+-----------------
+    8
+
+Remarque: si le numéro peux avoir plusieurs longueur, ajouter le du plus petit au plus grand ave '|' comme separateur
+
+    6|7|8|9
+
+Le code local
+-------------
+s'il existe 
+
+    0
+
+Remarque: s'il en a plusieurs, ajouter avec '|' comme separateur
+
+    0|1|2
+
+s'il existe pas la ligne se termie pas une 
+
+    ','
+
+au final on a cette ligne 
+
+    France,FR,+33|33|0033,8,0,
+
+
+
+### Remarque final
+
+    *Les espaces entre les valeur sont interdite
+    *La ligne finir toujour avec un ','
--- phone_email_verifier/treatment_phone.py
+++ phone_email_verifier/treatment_phone.py
@@ -14,7 +14,7 @@ except ImportError as IE:
 class Treatement_phone(Treatment):
     '''
     Object Treatement_phone
-    Qui faire le filtre sur les numéro de téléphone
+    Qui faire le filtre sur les numéros de téléphone
     '''
 
     def __init__(self, contacts, other=None):
@@ -28,26 +28,32 @@ class Treatement_phone(Treatment):
         self.len_phone = None
     
     def generate_phone_list(self):
-        old_contacts = [re.sub(r"[/ _-]", "", phone) for phone in self.contacts[:]]
+        old_contacts = [re.sub(r"[@#.!?/ _-]", "", phone) for phone in self.contacts[:]]
         for contact in old_contacts:
             phone = self.filter_phone(contact)
             if phone is not False:
-                self.new_contacts.append(self.add_code(phone))
+                number = self.add_code(phone)
+                if number not in self.new_contacts:
+                    self.new_contacts.append(number)
+
             else:
                 self.error_contacts.append(contact)
 
     def filter_by_all_country(self, contact):
         with open(func.get_dict_code_name(), encoding='utf-8') as country_info:
             for ligne in country_info:
-                regex = f"(^\\{ligne.split(',')[2]})"
-                reg = re.compile(regex)
-                if reg.match(contact) is not None:
+
+                lg = ligne.split(',')[:4]
+
+                regex = f"(^\\{lg[2]})"
+                
+                if re.match(regex, contact) is not None:
                     
                     try:
                         
-                        lg = ligne.split(',')[:4]
                         self.code = lg[2]
-                        regex = f"(^\\{lg[2]})(\d{ {int(lg[3])} }\Z)"
+
+                        regex = f"{regex}(\d{ {int(lg[3])} }\Z)"
 
                         r = re.match(regex, contact)
                         
@@ -75,25 +81,46 @@ class Treatement_phone(Treatment):
                             if r is not None:
                                 return r.groups()
 
-                        regex = f'^(\\{lg[2]})(\d{ {int(lg[3])} }\Z)'
+                        
+                        if '|' not in lg[3]:
+                            l = f'(\d{ {int(lg[3])} }\Z)'
+                        else:
+                            l = lg[3].split('|')
+                            l = f'(\d{ {int(l[:1][0]),int(l[-1:][0])} }\Z)'.replace(" ", "")
+                            
+
+                        regex = f'^(\\{lg[2]})' + l
+                        
                         r = re.match(regex, contact)
+
                         if r is not None:
                             return r.groups()
 
                         lg = ligne.split(',')[:5]
                         
                         if '\n' in lg:
-                            regex = f'(\d{ {int(lg[3])} }\Z)'
+                            regex = l
                             r = re.match(regex, contact)
                             if r is not None:
                                 return r.groups()
 
-                        regex = f'^({lg[4]})(\d{ {int(lg[3])} }\Z)'
-                        r = re.match(regex, contact)
-                        if r is not None:
-                            return r.groups()
+                        if '|' not in lg[4]:
+
+                            regex = f'^({lg[4]})' + l
+                            r = re.match(regex, contact)
+                            if r is not None:
+                                return r.groups()
+                            
+                        else:
+
+                            for p in lg[4].split('|'):
+                                regex = f'^({lg[4]})' + l
+                                r = re.match(regex, contact)
+                                if r is not None:
+                                    return r.groups() 
                         
                     except Exception as E:
+                        print(f'ERROR: {E}')
                         return False
 
             return False
@@ -115,6 +142,10 @@ class Treatement_phone(Treatment):
             return False
             
     def add_code(self, phone):
+
+        if '|' in self.code:
+            self.code = self.code.split('|')[0]
+
         if len(phone) > 1:
             if phone[0] == self.code:
                 return phone[0] + phone[1]
